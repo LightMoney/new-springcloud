2e678ff961c35ab0a5fcc485ec13ce862aa0a776
LightMoney <fan85719223@outlook.com>
2021-01-06 14:04:41 +0800

    gateway网关  自定义过滤器和自定义过滤器工厂

---
+++ gateway/src/main/java/cn/fan/gateway/filter/RequestTimeFilter.java
@@ -0,0 +1,42 @@
+package cn.fan.gateway.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 自定义过滤器（需要注入才能使用 详见启动类）
+ *
+ * @author fgy
+ * create 2021-1-6
+ **/
+public class RequestTimeFilter implements GatewayFilter, Ordered {
+
+    private static final Log log = LogFactory.getLog(GatewayFilter.class);
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+        return chain.filter(exchange).then(
+                Mono.fromRunnable(() -> {
+                    Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                    if (startTime != null) {
+                        log.info(exchange.getRequest().getURI().getRawPath() + ": " + (System.currentTimeMillis() - startTime) + "ms");
+                    }
+                })
+        );
+
+    }
+//优先级
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ gateway/src/main/java/cn/fan/gateway/filter/RequestTimeGatewayFilterFactory.java
@@ -0,0 +1,72 @@
+package cn.fan.gateway.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 自定义过滤器工厂（需要注入到ioc，并在配置文件中配置才生效）
+ *
+ **/
+public class RequestTimeGatewayFilterFactory extends AbstractGatewayFilterFactory<RequestTimeGatewayFilterFactory.Config> {
+
+
+    private static final Log log = LogFactory.getLog(GatewayFilter.class);
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+    private static final String KEY = "withParams";
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList(KEY);
+    }
+//用父类构造器将config类型传入
+    public RequestTimeGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+            return chain.filter(exchange).then(
+                    Mono.fromRunnable(() -> {
+                        Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                        if (startTime != null) {
+                            StringBuilder sb = new StringBuilder(exchange.getRequest().getURI().getRawPath())
+                                    .append(": ")
+                                    .append(System.currentTimeMillis() - startTime)
+                                    .append("ms");
+                            if (config.isWithParams()) {
+                                sb.append(" params:").append(exchange.getRequest().getQueryParams());
+                            }
+                            log.info(sb.toString());
+                        }
+                    })
+            );
+        };
+    }
+
+
+
+
+
+
+    public static class Config {
+
+        private boolean withParams;
+
+        public boolean isWithParams() {
+            return withParams;
+        }
+
+        public void setWithParams(boolean withParams) {
+            this.withParams = withParams;
+        }
+
+    }
+}
